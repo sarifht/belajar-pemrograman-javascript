c988a74d6bb89222cfdaddf4b01a089bff0bc571
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-23 06:36:38 +0700

    Add contoh for loop

---
+++ 1. Fundamental JavaScript/loop.js
@@ -0,0 +1,25 @@
+/*
+Sebelum adanya loop, kita harus menulis kode secara berulang-ulang untuk menampilkan angka 1 sampai 10 seperti dibawah ini, apalagi kalo dibungkus dengan variable harus diketik satu-satu.
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+*/
+
+// For Loop
+for(let i = 0; i < 5; i++) {
+    console.log(i);
+}
+/* output
+0
+1
+2
+3
+4
+*/
