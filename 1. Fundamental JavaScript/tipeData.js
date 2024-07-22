e4eaabb3fff736a3851f6f6af08be61d92ad9f1a
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-22 20:14:50 +0700

    Add Symbol

--- 1. Fundamental JavaScript/tipeData.js
+++ 1. Fundamental JavaScript/tipeData.js
@@ -122,3 +122,18 @@ console.log(someLaterData);
 /* output:
 null
 */
+
+
+// Tipe data Symbol
+const id = Symbol("id");
+console.log(id);
+/* output
+Symbol(id)
+*/
+
+const id1 = Symbol("id");
+const id2 = Symbol("id");
+console.log(id1 == id2);
+/* output
+false
+*/
