6f1f23e01ef1ab162dc0263ec07a6858cdfd29a2
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-23 04:50:18 +0700

    Add contoh if else statement

---
+++ 1. Fundamental JavaScript/ifElseStatement.js
@@ -0,0 +1,26 @@
+// Contoh If Else Statement > kondisi true
+const isRaining = true;
+
+console.log("Persiapan sebelum berangkat kegiatan.");
+if (isRaining) {
+    console.log("Hari ini hujan. Bawa payung.");
+}
+console.log("Berangkat kegiatan.");
+/* output:
+Persiapan sebelum berangkat kegiatan.
+Hari ini hujan. Bawa payung.
+Berangkat kegiatan.
+ */
+
+
+// Contoh If Else Statement > kondisi false
+let x = 50;
+
+if(x > 70) {
+    console.log(x);
+} else {
+    console.log("Nilai kurang dari 70");
+}
+/* output
+Nilai kurang dari 70
+*/
