1074839cce34635389d3ab0b9bd93c9776effb2d
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-22 20:20:40 +0700

    Add assignment operator dan comparison operator

---
+++ 1. Fundamental JavaScript/operator.js
@@ -0,0 +1,32 @@
+// Assignment Operator
+let x = 10;
+let y = 5
+
+x += y; // artinya -> x = x + y;
+
+console.log(x);
+/* output
+15
+*/
+
+
+// Comparison Operator
+let a = 10;
+let b = 12;
+
+console.log(a < b);
+console.log(a > b);
+/* output
+true
+false
+*/
+
+const aString = '10';
+const aNumber = 10
+
+console.log(aString == aNumber) // true, karena nilainya sama-sama 10
+console.log(aString === aNumber) // false, karena walaupun nilainya sama, tetapi tipe datanya berbeda
+/* output
+true
+false
+*/
