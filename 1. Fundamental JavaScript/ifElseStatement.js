a218debff9e556fcba49e6531f81191f1b7b304c
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-23 04:56:40 +0700

    Add ternary operator dan truthy falsy

--- 1. Fundamental JavaScript/ifElseStatement.js
+++ 1. Fundamental JavaScript/ifElseStatement.js
@@ -10,8 +10,7 @@ console.log("Berangkat kegiatan.");
 Persiapan sebelum berangkat kegiatan.
 Hari ini hujan. Bawa payung.
 Berangkat kegiatan.
- */
-
+*/
 
 // Contoh If Else Statement > kondisi false
 let x = 50;
@@ -23,4 +22,43 @@ if(x > 70) {
 }
 /* output
 Nilai kurang dari 70
+*/
+
+// Contoh If Else Statement > semua kondisi
+let language = "French";
+let greeting = "Selamat Pagi"
+
+if(language === "English") {
+    greeting = "Good Morning!";
+} else if(language === "French") {
+    greeting = "Bonjour!"
+} else if(language === "Japanese") {
+    greeting = "Ohayou Gozaimasu!"
+}
+
+console.log(greeting);
+/* output
+Bonjour!
+*/
+
+// Contoh ternary operator
+// condition ? true expression : false expression
+const isMember = false;
+const discount = isMember ? 0.1 : 0;
+console.log(`Anda mendapatkan diskon sebesar ${discount * 100}%`)
+
+/* output
+Anda mendapatkan diskon sebesar 0%
+*/
+
+// Truthy and Falsy
+let name = "";
+
+if (name) {
+    console.log(`Halo, ${name}`);
+} else {
+    console.log("Nama masih kosong");
+}
+/* output:
+Nama masih kosong
 */
