90de362875bba99a71b0d460964c95e5ca28b11f
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-25 00:29:58 +0700

    Add kondisi saat dan tidak menggunakan Spread Operator

--- 2. Struktur Data/spreadOperator.js
+++ 2. Struktur Data/spreadOperator.js
@@ -11,3 +11,23 @@ console.log(...favorites);
 /* output
 Seafood Salad Nugget Soup
 */
+
+// Saat kamua ga pake spread operator, akan seperti ini
+const others = ["Cake", "Pie", "Donut"];
+const allFavorites = [favorites, others];
+
+console.log(allFavorites);
+/* output
+[
+  [ 'Seafood', 'Salad', 'Nugget', 'Soup' ],
+  [ 'Cake', 'Pie', 'Donut' ]
+]
+*/
+
+// Saat menggunakan spread operator
+const allFavorites1 = [...favorites, ...others];
+
+console.log(allFavorites1);
+/* output
+[ 'Seafood', 'Salad', 'Nugget', 'Soup', 'Cake', 'Pie', 'Donut' ]
+*/
