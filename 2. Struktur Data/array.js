511654348a12b87e7a6f5dfb9cbca1a3c241a395
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-24 22:16:36 +0700

    Add kodingan array sampai pemanggilan index

---
+++ 2. Struktur Data/array.js
@@ -0,0 +1,19 @@
+let myArray = ["Cokelat", 42.5, 22, true, "Programming"];
+console.log(myArray);
+/* output:
+[ 'Cokelat', 42.5, 22, true, 'Programming' ]
+*/
+
+/* 
+Mirip sekali dengan object bukan? nah perbedaan array dan object ialah terletak pada susunan dan cara pemanggilannya, kalo ada array itu disusun secara berurutan dan dipanggil menggunakan index.
+
+contoh:
+console.log(myArray[1]);
+*/
+
+// Conth kode pemanggilan array menggunakan index
+let myArray1 = ["Cokelat", 42.5, 22, true, "Programming"];
+console.log(myArray1[1]);
+/* output:
+42.5
+*/
