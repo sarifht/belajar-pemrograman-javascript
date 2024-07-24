5d3ad89da1a2765793a75803a09822476590fa0c
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-25 00:26:32 +0700

    Add kode awal Spread Operator

---
+++ 2. Struktur Data/spreadOperator.js
@@ -0,0 +1,13 @@
+// Contoh Spread Operator
+/*
+Sesuai namanya “spread”, fitur ini digunakan 
+untuk menyebarkan nilai array atau lebih tepatnya 
+iterable object menjadi beberapa elemen. 
+Spread operator dituliskan dengan tiga titik (...). 
+*/
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+
+console.log(...favorites);
+/* output
+Seafood Salad Nugget Soup
+*/
