0802a774143231bf145b2c00001fffa94ef4298b
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-26 21:28:06 +0700

    Update kode destructuringObject.js

--- 2. Struktur Data/desctructuringObject.js
+++ 2. Struktur Data/desctructuringObject.js
@@ -5,9 +5,45 @@ const profile = {
     age: 18
   }
    
-  const { firstName, lastName, age } = profile;
+  const { firstName, middleName, lastName, age } = profile;
    
   console.log(firstName, lastName, age);
 /* output:
   John Doe 18
+*/
+
+// Destructuring Assignment
+/*
+Sebenernya disini ada materi tersebut, hanya saja karena 
+kodenya sama dengan yang diatas dan ada penambahan kode
+yang membuat kode tersebut menjadi error,
+saya lebih sarankan bikin file baru ya dan coba kamu
+pelajari dari W3Schools.
+*/
+
+// Default Values
+const profile2 = {
+    firs: "John",
+    last: "Doe",
+    years: 18
+  }
+   
+   
+  const { first, years, isMale } = profile2;
+   
+  console.log(first)
+  console.log(years)
+  console.log(isMale)
+/* output:
+  John
+  18
+  undefined
+*/
+
+/*
+Harusnya sih kode tersebut berjalan seperti output diatas,
+hanya saja dalam kode itu tidak diperbolehkan, tapi saat saya
+coba menggunakan compiler Dicoding, itu bisa dijalankan.
+Coba kamu pahami sendiri ya dan saran saya liat dokumentasinya
+di W3Schools.
 */
