9c3e8c52b6bda1c2d3ea04ac95b44991e5102cc4
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-26 22:22:35 +0700

    Map query dengan .has dan .delete

--- 2. Struktur Data/map.js
+++ 2. Struktur Data/map.js
@@ -34,3 +34,15 @@ console.log(capital.get("New Delhi"));
   4
   India
 */
+
+
+// Map query dengan .has dan .delete
+const wrongMap = new Map();
+wrongMap["My Key"] = "My Value";
+
+console.log(wrongMap.has("My Key"));
+console.log(wrongMap.delete("My Key"));
+/* output
+false
+false
+ */
