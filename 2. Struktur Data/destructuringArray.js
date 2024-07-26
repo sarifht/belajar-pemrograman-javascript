fe08d89f8de5ab9d4de0455f0a84935584644175
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-26 22:08:22 +0700

    Add contoh kodenya

---
+++ 2. Struktur Data/destructuringArray.js
@@ -0,0 +1,21 @@
+// Contoh kode Desctructuring Array
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+ 
+const [firstFood, secondFood, thirdFood, fourthFood] = favorites;
+ 
+console.log(firstFood);
+console.log(secondFood);
+console.log(thirdFood);
+console.log(fourthFood);
+/* output:
+Seafood
+Salad
+Nugget
+Soup
+*/
+
+/*
+Bedanya dengan Object, pada bagian tanda 
+kurung kurawal { } sedangkan array menggunakan 
+tanda kurung siku [ ]
+*/
