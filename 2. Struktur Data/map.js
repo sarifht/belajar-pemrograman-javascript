0801332d6001a527b6d756867e1e05c1e7ea9111
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-26 22:20:57 +0700

    Add kode inisial, lalu yang menggunakan set dan get

---
+++ 2. Struktur Data/map.js
@@ -0,0 +1,36 @@
+// Contoh kode Map
+const myMap = new Map([
+    ['1', 'a String key'],
+    [1, 'a number key'],
+    [true, true]
+  ]);
+  
+console.log(myMap);
+/* output
+  Map(3) { '1' => 'a String key', 1 => 'a number key', true => true }
+*/
+
+
+// Contoh kode Map dengan Get dan Set
+/*
+Ketika sudah membuat objek Map, kita bisa mendapatkan nilainya 
+berdasarkan key tertentu dengan metode get(). Lalu, untuk 
+menambahkan pasangan key-value baru gunakan metode set().
+*/
+const capital = new Map([
+    ["Jakarta", "Indonesia"],
+    ["London", "England"],
+    ["Tokyo", "Japan"]
+  ]);
+  
+console.log(capital.size);
+console.log(capital.get("London"));
+capital.set("New Delhi", "India");
+console.log(capital.size);
+console.log(capital.get("New Delhi"));
+/* output
+  3
+  England
+  4
+  India
+*/
