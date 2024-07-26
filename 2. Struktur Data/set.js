98e2aaf96cdd469ccf6c78468c2f7e8e0e69f735
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-26 22:28:01 +0700

    Menghapus nilai dari Set

--- 2. Struktur Data/set.js
+++ 2. Struktur Data/set.js
@@ -15,3 +15,11 @@ console.log(numberSet);
 /* output
 Set(5) { 1, 4, 6, 5, 10 }
 */
+
+// Menghapus nilai dari Set
+numberSet.delete(4);
+
+console.log(numberSet);
+/* output
+Set(4) { 1, 6, 5, 10 }
+*/
