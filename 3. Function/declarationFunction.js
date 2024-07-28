f063b32b2cc44ef4a8011e06cabb7160800c6731
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-07-28 09:40:22 +0700

    Add fungsi dengan if else statement

--- 3. Function/declarationFunction.js
+++ 3. Function/declarationFunction.js
@@ -7,3 +7,21 @@ function greeting() {
 /* output
   Good Morning!
 */
+
+
+// Fungsi dengan if-else statement
+function sapaan(nama, bahasa) {
+    if(bahasa === "Indonesia") {
+        console.log('Selamat Pagi ' + nama + '!');
+    } else if (bahasa === "English") {
+        console.log(`Good Morning ${nama}!`);
+    } else {
+        console.log(`Kumaha damang ${nama}!`);
+    }
+}
+  
+sapaan("Sarif", "Indonesia");
+/* output
+  Bonjour Harry!
+*/
+
