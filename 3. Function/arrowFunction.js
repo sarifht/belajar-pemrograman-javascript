0dba6533a4901ef3b87cc0e2fe9841847068ff33
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-01 17:15:49 +0700

    Add arrow function, satu parameter dan non parameter

---
+++ 3. Function/arrowFunction.js
@@ -0,0 +1,13 @@
+// Hanya memiliki satu parameter
+const sayName = (name) => {
+    console.log(`Halo nama saya ${name}`);
+}
+
+sayName('Sarif Hidayatullah');
+
+// Jika tidak membutuhkan parameter
+const sayHello = () => {
+    console.log('Selamat sore!');
+}
+
+sayHello();
