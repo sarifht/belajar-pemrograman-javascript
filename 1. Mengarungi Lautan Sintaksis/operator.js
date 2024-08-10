4f837c9c2225e099070938f3fe967648ff9c1d58
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-10 14:38:10 +0700

    Add unary, binary dan ternary

---
+++ 1. Mengarungi Lautan Sintaksis/operator.js
@@ -0,0 +1,34 @@
+// Belajar Operator di JavaScript
+// 5 + 5; //angka 5 merupakan operand dan + merupakan operator
+
+/* 
+    Secara umum ada 3 kategori operator di JavaScript
+    1. Unary Operator
+    2. Binary Operator
+    3. Ternary Operator
+*/
+
+let age = 25;
+ 
+// Unary operator
+typeof age;
+ 
+// Binary operator
+5 + 4;
+10 / 2;
+age = 30;
+ 
+// Ternary operator (conditional operator)
+(age < 18) ? 'You are too young!' : 'Welcome onboard!'; 
+
+// Tidak menampilkan apa-apa, hanya contoh operator sesuai kategorinya.
+
+/* 
+    Ada beberapa jenis operator di JavaScript
+    1. Assignment Operator (Operator Penugasan)
+    2. Arithmetic Operator (Operator Aritmatika)
+    3. Comparison Operator (Operator Perbandingan)
+    4. Logical Operator (Operator Logika)
+    5. String Operator (Operator String)
+*/
+
