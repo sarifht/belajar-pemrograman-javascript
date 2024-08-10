917187d241271046e22a091b074db8aed9660d98
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-10 09:45:58 +0700

    Add file comment.js

---
+++ 1. Mengarungi Lautan Sintaksis/comment.js
@@ -0,0 +1,21 @@
+// Belajar Comment
+// Ini adalah contoh komentar dalam satu baris
+
+console.log('Hai kamu!'); 
+// Komentar tidak ada tampil jika dijalankan pada interpreter JavaScript
+console.log('Aku Sarif Hidayatullah');
+
+// Shortcut komentar satu baris di VS Code: Ctrl + /
+/* 
+    Ini adalah contoh komentar
+    lebih dari satu baris menggunakan /*
+    dan tidak akan ditampilkan pada hasil eksekusi
+    
+    Shorcut komentar lebih dari satu baris di VS Code: Shift + Alt + A
+*/
+
+const PI = 3.14;
+console.log(PI);
+
+// Komentar satu baris disebut single line comment
+// Komentar lebih dari satu baris disebut multi line comment
