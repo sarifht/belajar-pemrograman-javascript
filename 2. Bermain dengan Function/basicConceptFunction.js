2c2760ffda9fa826571f83b25ca248ecdcfc0c09
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-12 08:10:24 +0700

    Add kode tanpa function-1

---
+++ 2. Bermain dengan Function/basicConceptFunction.js
@@ -0,0 +1,8 @@
+// Implementasi Konversi Suhu tanpa Function
+const temperatureInCelsius = 90;
+const temperatureInFahrenheit = (9 / 5) * temperatureInCelsius + 32;
+
+console.log("Hasil konversi:", temperatureInFahrenheit);
+/* Output:
+Hasil konversi: 194
+*/
