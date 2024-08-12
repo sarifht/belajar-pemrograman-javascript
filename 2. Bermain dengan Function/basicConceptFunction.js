5a4b16baf77d54a44092acbae481d00bd3a482f7
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-12 09:06:16 +0700

    Add kode setelah menggunakan function

--- 2. Bermain dengan Function/basicConceptFunction.js
+++ 2. Bermain dengan Function/basicConceptFunction.js
@@ -6,3 +6,15 @@ console.log("Hasil konversi:", temperatureInFahrenheit);
 /* Output:
 Hasil konversi: 194
 */
+
+// Setelah menggunakan Function
+function convertCelsiusToFahrenheit(temperature) {
+  const temperatureInFahrenheit = (9 / 5) * temperature + 32;
+
+  console.log("Hasil konversi:", temperatureInFahrenheit);
+}
+
+convertCelsiusToFahrenheit(90);
+/* Output:
+Hasil konversi: 194
+*/
