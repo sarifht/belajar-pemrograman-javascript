c946408a2f2d306fb4cb6e4c1985e65007355878
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-18 19:44:21 +0700

    Add object.js dan menghapus index.js di folder Bukan Sembarang Data

--- 3. Bukan Sembarang Data/index.js
+++
@@ -1,4 +0,0 @@
-console.log("Object");
-console.log("Array");
-console.log("Map");
-console.log("Set");
---
+++ 3. Bukan Sembarang Data/object.js
@@ -0,0 +1,19 @@
+// Kodingan dasar dari Object
+const user = {
+  name: "Dicoding",
+  "last name": "Indonesia",
+  age: 9,
+};
+
+console.log(user.name); // Output: Dicoding
+
+// Mencoba membuat kodingan Object sendiri
+const nama = {
+  presiden: "Jokowi",
+  anakPresiden: "Gibran",
+  abah: "Anies",
+  hasil: 17,
+  "Nama saya": "Sarif",
+};
+
+console.log(nama);
