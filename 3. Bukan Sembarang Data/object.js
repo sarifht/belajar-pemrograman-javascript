32e0f6358b7cd98f3462375361835eeeff146370
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-19 11:14:41 +0700

    Memanggil via square bracker, belajar destructuring dan memanggil nilai yang tidak ada dalam object

--- 3. Bukan Sembarang Data/object.js
+++ 3. Bukan Sembarang Data/object.js
@@ -13,7 +13,28 @@ const nama = {
   anakPresiden: "Gibran",
   abah: "Anies",
   hasil: 17,
-  "Nama saya": "Sarif",
+  namaDepan: "Sarif",
+  namaBelakang: "Hidayatullah",
+  Hewan: "",
 };
 
 console.log(nama);
+
+// Memanggil data menggunakan square bracket
+console.log(nama["Nama saya"]); // Output: Sarif
+console.log(nama["anakPresiden"]); // Output: Gibran
+
+// Memanggil data dengan cara Desctructuring
+const { namaDepan, namaBelakang } = nama;
+console.log(namaDepan, namaBelakang); // Output: Sarif Hidayatullah
+
+// // Jika memanggil tanpa Desctructuring -> hilangkan komentar kode dibawah ini
+// Berikan komentar pada kode Desctructuring di atas, lalu jalankan kode berikut:
+// console.log("");
+// const namaDepan = nama.namaDepan;
+// const namaBelakang = nama.namaBelakang;
+// console.log(namaDepan, namaBelakang); // Output: Sarif Hidayatullah
+
+// Jika memanggil data yang tidak ada di Object
+const hewan = { karnivora: "Singa" };
+console.log(hewan.herbivora); // Output: undefined
