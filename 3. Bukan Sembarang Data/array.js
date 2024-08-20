a3e717d932d3f676b3ac2cb82e3a3b7d45f98a53
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-20 14:57:35 +0700

    Add konsep dasar Array

---
+++ 3. Bukan Sembarang Data/array.js
@@ -0,0 +1,19 @@
+// Perkenalan dengan Array
+const array = [1, 2];
+console.log(typeof array); // Output: object
+
+/* 
+    Perbedaan antara Array dan Object adalah
+    pada posisi urutan datanya, yang mana
+    pada Array lebih terurut, tapi pada dasarnya
+    Array adalah object yang memiliki urutan saja.
+
+    Versi lebih rapinya kalo boleh dibilang.
+*/
+
+// Penulisan Array
+const nama = ["Sarif", "Gibran", "Anies", "Jokowi", "Mahfud"];
+
+console.log(nama);
+console.log(nama[0]);
+console.log(nama[9]);
