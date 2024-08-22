4d212feecbacca93a970ca8f9ae6d2fe854a5c17
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-22 22:01:21 +0700

    Add for loop dan for in

--- 4. Sang Pengatur Program/looping.js
+++ 4. Sang Pengatur Program/looping.js
@@ -9,3 +9,22 @@ const makanan = ["Nasi Goreng", "Mie Ayam", "Bakso", "Soto"];
 for (let i = 0; i < makanan.length; i++) {
   console.log(makanan[i]);
 }
+
+// Kodingan for loop
+for (let i = 0; i < 5; i++) {
+  console.log(`Angka ke-${i} adalah ${i}`);
+}
+
+// Kodingan for in
+console.log("");
+const person = { name: "Sarif", origin: "Banten", birthYear: 2024 };
+
+for (const property in person) {
+  console.log(`${property} bernilai ${person[property]}`);
+}
+
+console.log("");
+const namaOrang = { nama1: "Sarif", nama2: "Anies", nama3: "Joko" };
+for (const panggilan in namaOrang) {
+  console.log(`Halo nama saya ${namaOrang[panggilan]}`);
+}
