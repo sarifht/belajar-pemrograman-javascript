4cf62759e4f1763a59a390097511fc5bebd14b33
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-22 21:34:38 +0700

    Add ifStatement.js

---
+++ 4. Sang Pengatur Program/ifStatement.js
@@ -0,0 +1,27 @@
+// If statement sederhana
+
+const nilaiBagus = 80;
+
+if (nilaiBagus > 70) {
+  console.log("Selamat kamu lulus");
+}
+console.log("Program selesai");
+
+const nilaiJelek = 50;
+if (nilaiJelek > 70) {
+  console.log("Selamat kamu lulus");
+} else {
+  console.log("Maaf kamu belum lulus");
+}
+console.log("Program selesai");
+
+// Menggunakan if else
+const nilai = 75;
+
+if (nilai >= 80) {
+  console.log("Selamat, nilai Kamu A");
+} else if (nilai >= 70) {
+  console.log("Selamat, nilai kamu B");
+} else {
+  console.log("Kamu mendapat nilai C");
+}
