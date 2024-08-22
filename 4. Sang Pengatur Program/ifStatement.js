2db7b38b05849006157471d7bfa52ab6b9b97178
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-22 21:39:52 +0700

    Add shortcut if statement dari Dicoding

--- 4. Sang Pengatur Program/ifStatement.js
+++ 4. Sang Pengatur Program/ifStatement.js
@@ -31,3 +31,10 @@ const anggota = 12;
 const teamFootball = false;
 const jumlah = teamFootball ? 0 : -1;
 console.log(`Jumlah anggota tim adalah ${anggota + jumlah}`);
+
+// Kodingan dari Dicoding
+const price = 100000;
+const isMember = true;
+const discount = isMember ? 0.1 : 0;
+
+console.log(`Anda mendapatkan discount sebesar ${discount * price}`);
