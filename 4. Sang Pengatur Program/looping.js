3ea05f665025d83bea57ba7ece553df36261e74c
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-22 21:55:45 +0700

    Add looping menggunakan for

---
+++ 4. Sang Pengatur Program/looping.js
@@ -0,0 +1,11 @@
+// Kondisi tanpa looping
+const makanan = ["Nasi Goreng", "Mie Ayam", "Bakso", "Soto"];
+// console.log(makanan[0]);
+// console.log(makanan[1]);
+// console.log(makanan[2]);
+// console.log(makanan[3]);
+
+// Mencoba looping -> for
+for (let i = 0; i < makanan.length; i++) {
+  console.log(makanan[i]);
+}
