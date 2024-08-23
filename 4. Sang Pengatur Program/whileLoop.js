070aede800660928f571034bea895cf04c113993
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-23 11:34:14 +0700

    Add whileLoop.js

---
+++ 4. Sang Pengatur Program/whileLoop.js
@@ -0,0 +1,12 @@
+let i = 0;
+
+while (i < 6) {
+  console.log(`Sekarang ada i ke-${i}`);
+  i++;
+}
+
+/* 
+    Harus berhati-hati dalam menuliskan looping ini,
+    karena jika tidak ada penambahan nilai i, 
+    maka akan terjadi infinite loop
+*/
