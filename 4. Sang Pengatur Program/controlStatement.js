40c15a7ddc6cec5256416fd1ed903931858f954b
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-24 12:46:30 +0700

    Add controlStatement.js

---
+++ 4. Sang Pengatur Program/controlStatement.js
@@ -0,0 +1,18 @@
+// Break
+for (i = 0; i < 10; i++) {
+  if (i === 7) {
+    break;
+  }
+  console.log(i);
+}
+console.log("i akan berhenti sebelum angka 7");
+console.log("");
+
+// Continue
+for (i = 0; i < 10; i++) {
+  if (i === 7) {
+    continue;
+  }
+  console.log(i);
+}
+console.log("i akan melewati angka 7");
