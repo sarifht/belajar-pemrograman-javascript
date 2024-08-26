5d135a46fad87c81b8cdc4b3ca02773ff88994cd
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-08-26 18:54:59 +0700

    Add kode error untuk throwing dan catching error

--- 4. Sang Pengatur Program/errorHandling.js
+++ 4. Sang Pengatur Program/errorHandling.js
@@ -1,3 +1,20 @@
 // Membangkitkan pesan error
 const error = new Error("Terjadi error");
 console.error(error);
+
+// Throwing error
+const price = 100;
+const paid = 80;
+
+if (paid < price) {
+  throw new Error("Pembayaran kurang");
+}
+
+// Catching error
+try {
+  console.log("Memulai program");
+  throw new Error("Error: Program berhenti");
+  console.log("Mengakhiri program");
+} catch (err) {
+  console.log("Karena ada error, blok ini akan dieksekusi");
+}
