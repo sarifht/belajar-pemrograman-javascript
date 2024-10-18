1be74d8379a9ed69eea5566b2b0412f3b733dfca
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-10-18 23:32:52 +0700

    Update data import

--- 5. Modularisasi Harmonis/anotherFile.mjs
+++ 5. Modularisasi Harmonis/anotherFile.mjs
@@ -1,3 +1,9 @@
 export default function myFunction() {
   console.log("Ini adalah function export default.");
 }
+
+const name = "John";
+const email = "john@gmail.com";
+const age = 25;
+
+export { name, email, age };
--- 5. Modularisasi Harmonis/main.mjs
+++ 5. Modularisasi Harmonis/main.mjs
@@ -1,3 +1,7 @@
 import myFunction from "./anotherfile.mjs";
+import { name, email, age } from "./anotherfile.mjs";
 
 myFunction();
+console.log(name);
+console.log(email);
+console.log(age);
